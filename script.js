2c1764f05fe4c468d08e5485befbbdee448c2a88
ashida-fathima <ashida072@gmail.com>
2026-01-10 17:15:08 +0530

    Initial commit: add problem statement webpage

---
+++ script.js
@@ -0,0 +1,6 @@
+function highlightText() {
+    const text = document.getElementById("problemText");
+    text.style.backgroundColor = "#fff3cd";
+    text.style.borderLeft = "5px solid #ffc107";
+    text.style.padding = "10px";
+}
